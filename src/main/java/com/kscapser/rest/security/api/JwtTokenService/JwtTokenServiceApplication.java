67bd537434bae67812b1950c39449ccd60b441b1
sunilrai <sunilrai7607@gmail.com>
2020-07-31 13:43:57 -0400

    initial commit

---
+++ src/main/java/com/kscapser/rest/security/api/JwtTokenService/JwtTokenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kscapser.rest.security.api.JwtTokenService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kscapser/rest/security/api/JwtTokenService/JwtTokenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kscapser.rest.security.api.JwtTokenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
